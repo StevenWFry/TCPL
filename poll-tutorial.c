2ea2905e51e05ab6f7d27894090e26a144b62dac
Steven Fry <sw.fry@outlook.com>
2022-07-22 20:32:54 -0400

    Added poll tutorial, it helped

---
+++ poll-tutorial.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <poll.h>
+
+int main(int argc, char *argv) {
+    char name[255];
+    int counter =0;
+    
+    struct pollfd mypoll;
+
+    /* Initialize mypoll */
+    memset(&mypoll, 0, sizeof(mypoll));
+    mypoll.fd = 0;
+    mypoll.events = POLLIN;
+
+    printf("Type in your name:\n");
+    
+    while(1){
+        if(poll(&mypoll, 1, 100) == 1) {
+            read(0, name, sizeof(name));
+            printf("Hello, %s", name);
+            break;
+        }
+        else
+            counter++;
+    }
+
+    printf("It took you %d ms to type in your name\n", 100 * counter);
+
+    return 0;
+}
